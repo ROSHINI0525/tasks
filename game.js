dfe7c4ce09904d7eb6be81a31bf121851b3e7f5c
ROSHINI0525 <111134696+ROSHINI0525@users.noreply.github.com>
2022-08-19 12:07:26 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,38 @@
+const player= prompt("choice:");
+console.log("options:"+ player);
+console.log("Opponent choice:")
+let Opponent=Math.floor(Math.random() * 3);
+if (Opponent==0){
+Opponent="stone";
+}
+else if (Opponent==1) {
+Opponent="paper";
+}
+else {
+Opponent="scissor";
+}
+console.log(Opponent);if (You==Opponent)
+{
+console.log("tie");
+}
+else if(player=="stone"&&Opponent=="paper"){
+console.log("Opponent wins!");
+}
+else if(player=="stone"&&Opponent=="scissor"){
+console.log("player wins!");
+}
+else if(player=="paper"&&Opponent=="scissor"){
+console.log("Opponent wins!");
+}
+else if(player=="paper"&&Opponent=="stone"){
+console.log("player wins!");
+}
+else if(player=="scissor"&&Opponent=="paper"){
+console.log("player wins!");
+}
+else if(player=="scissor"&&Opponent=="stone"){
+console.log("Opponent wins!");
+}
+else{
+console.log("please enter a valid input");
+}
