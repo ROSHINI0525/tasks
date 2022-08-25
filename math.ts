edd75a31b873c4229cec9ace6a6dd02e799d7b05
ROSHINI0525 <111134696+ROSHINI0525@users.noreply.github.com>
2022-08-25 11:22:32 +0530

    Add files via upload

---
+++ math.ts
@@ -0,0 +1,15 @@
+let num1: number = 5;
+let num2: number = 4;
+let num3: number = 25;
+
+let add = num1+num3;
+console.log(add)
+
+let sub = num2-num3;
+console.log(sub)
+
+let mul = num2*num3;
+console.log(mul)
+
+let div= num2/num3;
+console.log(div)
